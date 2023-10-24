83508c68a43d0af55619bd0a7de2ece814ce8e64
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2023-10-24 17:05:51 +0200

    Create the necessary files.

---
+++ saves/search.py
@@ -0,0 +1,22 @@
+import requests
+
+class Searcher:
+    def search(query):
+        query = query.strip()
+        query = query.lower()
+        
+        
+        
+        
+        response = requests.get(url)
+        if not response.status_code == 200:
+            raise Exception
+        
+        
+    
+        
+
+
+
+if __name__ == "__main__":
+    ...
