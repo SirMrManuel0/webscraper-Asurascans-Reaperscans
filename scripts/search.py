a40529b60901f35c8998e5f3011f6fe3a0c41865
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2023-10-25 13:37:39 +0200

    Implemented optimized search for AsuraScans

--- scripts/search.py
+++ scripts/search.py
@@ -31,7 +31,7 @@ def google_search(query:str, amount = 1):
 
 def search_asurascans(query:str):
     """
-    Perform a search for a certain manga / manhua / manhwa on asurascans.
+    Perform a search for a certain manga / manhua / manhwa on AsuraScans.
     
     Args:
         query (str): The name.
@@ -39,109 +39,25 @@ def search_asurascans(query:str):
     Returns:
         dict: A dictonary of results (name: url).
     """
-    
-    # Get header from config.json
-    with open('config.json', 'r') as config_file:
-        config = json.load(config_file)
-
-    # Extract the headers from the configuration
-    headers = config.get("headers", {})
-    
-    # Replace spaces with plus signs to format the query for the URL
-    query = query.replace(" ", "+")
-    
     # Read JSON file data
-    with open("saves/asura/asura.json", 'r') as json_file:
+    with open("scripts/search_asura_cache.json", 'r') as json_file:
         data_asura = json.load(json_file)
     
-    # Construct the URL for the search
-    url = data_asura["url"]
-    url += "?s=" + query
     
     
-    # Send a GET request to the AsuraScans search page
-    response = requests.get(url,headers=headers)
+    temp_dict = data_asura.copy()
     
-    # Extract the page numbers from the HTML content
-    page_number = response.text.split("\n")
-    page_number = [i for i in page_number if i.find('<a class="page-numbers"') > -1]
+    # Filter comics by query
+    for k,i in temp_dict.items():
+        if k.lower().find(query.lower()) < 0:
+            data_asura.pop(k)
     
-    try:
-        # Get the last page number if it exists
-        page_number = page_number[len(page_number)-1]
-    except IndexError:
-        try:
-            # Get the first page number if the last page number doesn't exist
-            page_number = page_number[0]
-        except IndexError:
-            # Use a default page number if no page numbers are found
-            page_number = "<a>1</a>"
-            
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(page_number, 'html.parser')
-    
-    # Find the text inside the <a> tag to determine the total number of pages
-    page_number = soup.get_text()
-    page_number = int(page_number) + 1 # Add 1 to loop through all pages with range()
-    
-    returner = {}
-    
-    # Iterate through pages
-    for i in range(1, page_number):
-        
-        
-        if i == 1:
-            url = data_asura["url"]
-            url += "?s=" + query
-        else:
-            url = data_asura["url"]
-            url += "page/" + str(i) + "/?s=" + query
-        
-        # Send a GET request to the AsuraScans search page
-        response = requests.get(url,headers=headers)
-        
-        # Split the HTML content into lines
-        lines = response.text.split("\n")
-        
-        
-        url = data_asura["url"]
-        
-        
-        # Extract links to manga titles from the HTML content
-        links = [i for i in lines if i.find(f'<a href="{url}manga/') > -1]
-        
-        results = []
-
-        # Extract URL and title from each link using regular expressions
-        for link in links:
-            match = re.search(r'href="([^"]+)" title="([^"]+)"', link)
-            
-            if match:
-                url = match.group(1)
-                title = match.group(2)
-                results.append(f"{url} {title}")
-        
-        # Create a dictionary of results (name: url)
-        for i in results:
-            splitted = i.split(" ")
-            url = splitted[0]
-            length = len(splitted)
-            name = ""
-            for i in range(1, length):
-                if not i == length - 1:
-                    name += splitted[i] + " "
-                elif i == length - 1:
-                    name += splitted[i]
-                    
-            name = html.unescape(name)  # Replace HTML entities
-            returner[name] = url
-    
-    return returner
+    return data_asura
 
 
 def search_reaperscans(query:str):
     """
-    Search for comics on the Reaperscans website and filter by a given query.
+    Search for comics on the ReaperScans website and filter by a given query.
 
     Args:
     query (str): The query to filter comics by.
